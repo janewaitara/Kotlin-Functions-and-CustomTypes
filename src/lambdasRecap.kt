ae8ae84ce82b9554a77d699e490df1a91e22c586
janewaitara <janewaitara99@gmail.com>
2020-06-03 03:05:06 +0300

    lambda recap

---
+++ src/lambdasRecap.kt
@@ -0,0 +1,24 @@
+
+// anonymous functions
+fun main() {
+
+    fun operateOnNumbers(a: Int, b: Int, operation: (Int,Int) -> Int): Int{
+        return  operation(a,b)
+    }
+
+    val addLambda = {a:Int, b: Int ->
+       a + b
+    }
+
+    operateOnNumbers(4,2,operation = addLambda)
+    //ca be defined in line
+
+    operateOnNumbers(2,5){a,b -> a+b}
+
+    fun addFunction(a: Int , b: Int) = a + b
+    operateOnNumbers(4,2,::addFunction)
+    //using function reference for the plus operator
+    operateOnNumbers(5,8,Int::plus) //since a plus takes in two numbers and returns one number
+
+
+}
